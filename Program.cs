0d4c39c84e6ea0fc2e8e2f02dc48bb93469781e2
HaYeonKim0807 <kh701201@naver.com>
2021-03-05 21:06:01 +0900

    message

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodOut
+{
+    class Program
+    {
+        static void Divide( ref int a, ref int b, out int result1, out int result2 )
+        {
+            result1 = a / b;
+            result2 = a % b;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int num1 = 10;
+            int num2 = 3;
+
+            int result1;
+            int result2;
+
+            Divide(ref num1, ref num2, out result1, out result2);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+        }
+    }
+}
